1875d9763df7bfb2a21b5c510dbf46decaa99857
Xiao Chen <xiao@cloudera.com>
2016-12-20 14:24:22 -0800

    add step5
    Change-Id: Id9b71e5757828957498a4827da75f21d9eeeb133

--- dev-support/license/step5.sh
+++ dev-support/license/step5.sh
@@ -2,5 +2,11 @@
 
 SCRIPT_DIR=$(dirname "${BASH_SOURCE-$0}")
 pushd $SCRIPT_DIR/../../
-for f in $(grep -r -i copyright . | awk -F ":" '{print $1}' |uniq ) ;do grep -L "http://www.apache.org/licenses/LICENSE-2.0" "$f";done > copyright.txt 
+
+STAGING_FILE=files_with_copyright.txt
+grep -r -i -E "copyright|©" . | awk -F ":" '{print $1}' |uniq > $STAGING_FILE
+OLDIFS=$IFS
+IFS=$'\n'
+for f in $(cat $STAGING_FILE) ;do grep -L "http://www.apache.org/licenses/LICENSE-2.0" "$f";done > copyright.txt
+IFS=$OLDIFS
 popd
